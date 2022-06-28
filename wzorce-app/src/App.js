161c214f41ec291ea1ddc1760f373324ba4519da
mateu52 <wuoelte@gmail.com>
2022-06-28 09:05:12 +0200

    zad 2 HOC

--- wzorce-app/src/App.js
+++ wzorce-app/src/App.js
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Content from './component/Content';
-//import Passphrase from './component/Passphrase';
-import Movies from './component/Movies';
-import Places from './component/Places';
-// import { BrowserRouter as Router , Link, Routes, Route } from "react-router-dom";
 
 
 function App() {
@@ -35,28 +31,7 @@ function App() {
       <MyContext.Provider value = { klucz }>
           <Passphrase />
       </MyContext.Provider>
-      {/* <Router>
-                
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/movies">Movies</Link>
-                            </li>
-                            <li>
-                                <Link to="/places">Places</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <Routes>
-                        <Route path="/" element={<Content />} />
-                        <Route path="/movies" element={<Movies />} />
-                        <Route path="/places" element={<Places />} />
-                    </Routes>
-                
-            </Router> */}
+    
     </div>
   )
 };
--- wzorce-app/src/component/Content.js
+++ wzorce-app/src/component/Content.js
@@ -1,15 +1,25 @@
 import React from 'react';
-// import Movies from './Movies';
-// import Places from './Places';
-
+import Movies from './Movies';
+import Places from './Places';
+import { BrowserRouter as Router , Link, Routes, Route } from "react-router-dom";
 function Content(){
     return(
-        <div>
-            <h3>Witam na stronie</h3>
-            
-
-
-        </div>
+        <Router>
+            <nav>
+                <ul>
+                <li>
+                    <Link to="/movies">Movies</Link>
+                </li>
+                <li>
+                    <Link to="/places">Places</Link>
+                </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/places" element={<Places />} />
+                <Route path="/movies" element={<Movies />} />
+            </Routes>
+        </Router>
     )
 };
 
